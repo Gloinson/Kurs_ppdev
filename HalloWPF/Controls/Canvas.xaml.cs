620919b666f5a4d6222f30557d39e15891221737
Christian Georg Becker <chg.becker@gmail.com>
2017-07-18 10:25:57 +0200

    Ctls in Window

--- HalloWPF/Controls/Canvas.xaml.cs
+++ HalloWPF/Controls/Canvas.xaml.cs
@@ -18,9 +18,9 @@ namespace HalloWPF.Controls
     /// <summary>
     /// Interaction logic for UserControl1.xaml
     /// </summary>
-    public partial class UserControl1 : UserControl
+    public partial class Canvas : UserControl
     {
-        public UserControl1()
+        public Canvas()
         {
             InitializeComponent();
         }
