00118ec96165f68f543caf58235a4c2974353feb
Christian Georg Becker <chg.becker@gmail.com>
2017-07-20 11:43:39 +0200

    dependency props example

---
+++ Properties_Dependency/TextLabel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace Properties_Dependency
+{
+    class TextLabel : FrameworkElement
+    {
+        public static readonly DependencyProperty TextProperty;
+        public static readonly DependencyProperty FontSizeProperty;
+
+        static TextLabel()
+        {
+            TextProperty = DependencyProperty.Register(
+                "Text",
+                typeof(string),
+                typeof(TextLabel),
+                new FrameworkPropertyMetadata(
+                    defaultValue: string.Empty,
+                    flags: FrameworkPropertyMetadataOptions.AffectsMeasure |
+                           FrameworkPropertyMetadataOptions.AffectsRender));
+            FontSizeProperty = DependencyProperty.Register(
+                "Fontsize",
+                typeof(double),
+                typeof(TextLabel),
+                new FrameworkPropertyMetadata(
+                    defaultValue: string.Empty,
+                    flags: FrameworkPropertyMetadataOptions.AffectsMeasure |
+                           FrameworkPropertyMetadataOptions.AffectsRender));
+        }
+
+        public string Text
+        {
+            get
+            {
+                return (string)GetValue(TextProperty);
+            }
+            set
+            {
+                SetValue(TextProperty, value);
+            }
+        }
+
+        public double FontSize
+        {
+            get
+            {
+                return (double)GetValue(FontSizeProperty);
+            }
+            set
+            {
+                SetValue(FontSizeProperty, value);
+            }
+        }
+    }
+}
