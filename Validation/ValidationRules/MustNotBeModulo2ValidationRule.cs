3c7ac4fa1b54eec14ea1189d1843406bec1c2b06
Christian Georg Becker <chg.becker@gmail.com>
2017-07-20 14:18:09 +0200

    validationrule added

---
+++ Validation/ValidationRules/MustNotBeModulo2ValidationRule.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace Validation.ValidationRules
+{
+    public class MustNotBeModulo2ValidationRule : ValidationRule
+    {
+        public override ValidationResult Validate(object value, CultureInfo cultureInfo)
+        {
+            if(value is double c)
+            {
+                var i = (int)c;
+                if ((i % 2) == 0)
+                    return new ValidationResult(false, "Value must not be modulo 2.");
+            }
+            return ValidationResult.ValidResult;
+        }
+    }
+}
