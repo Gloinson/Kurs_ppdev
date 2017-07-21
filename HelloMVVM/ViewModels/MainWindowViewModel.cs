05fa30c99da83e1f88bc915cd153f6289171e6e1
Christian Georg Becker <chg.becker@gmail.com>
2017-07-21 12:05:34 +0200

    CallerMemberName and other gadgets

--- HelloMVVM/ViewModels/MainWindowViewModel.cs
+++ HelloMVVM/ViewModels/MainWindowViewModel.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,14 +10,25 @@ namespace HelloMVVM.ViewModels
 {
     internal class MainWindowViewModel : INotifyPropertyChanged
     {
+        private void RaisePropertyChanged([CallerMemberName] string param = null)
+        {
+            // to avoid threading-problems
+            // does not solve problem with self-disposed event-receiver
+            var threadtmp = PropertyChanged;
+            if (threadtmp != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(param));
+
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(param));
+        }
+
         private string _welcomeText = "Hallo MVVM!";
-        public string WelcomeText {
+        public string WelcomeText
+        {
             get => _welcomeText;
-            set {
+            set
+            {
                 _welcomeText = value;
-                var threadtmp = PropertyChanged;
-                if(threadtmp != null)
-                    PropertyChanged(this, new PropertyChangedEventArgs("WelcomeText"));
+                RaisePropertyChanged();
             }
         }
 
