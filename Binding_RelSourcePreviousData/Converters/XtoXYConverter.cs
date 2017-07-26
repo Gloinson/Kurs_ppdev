48905926ef4e9c4e9e2c7574d969fce4ce9b7851
Christian Georg Becker <chg.becker@gmail.com>
2017-07-26 11:30:29 +0200

    multiconverter für datatemplate test

--- Binding_RelSourcePreviousData/Converters/XtoXYConverter.cs
+++ Binding_RelSourcePreviousData/Converters/XtoXYConverter.cs
@@ -4,19 +4,43 @@ using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
 using System.Windows.Data;
 
 namespace Binding_RelSourcePreviousData.Converters
 {
-    class XtoXYConverter : IValueConverter
+    internal class XtoPointConverter : IMultiValueConverter
     {
-        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
         {
-            int xval = int.Parse(value as string);
-            return $"0 {xval}";
+            int xval = (int)value[0] * (int)(double)value[1];
+
+            int yval = 0;
+            if (value[2] is int)
+                yval = (int) value[2];
+
+            return new Point(yval, xval);
         }
 
-        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+    internal class XtoXYConverter : IMultiValueConverter
+    {
+        public object Convert(object[] value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int xval = (int)value[0] * (int)(double)value[1];
+
+            int yval = 0;
+            if (value[2] is int)
+                yval = (int)value[2];
+
+            return $"{yval}, {xval}";
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
